65a94b5b76a08abb87f0e1835c4ef12e11a84d85
Art <teamkait1@gmail.com>
2025-03-31 21:12:21 +0300

    GetIdByName

--- Classes/DBHelper.cs
+++ Classes/DBHelper.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -25,6 +26,35 @@ namespace Sports.Classes
             }
 
             return property.GetValue(entity)?.ToString();
+        }
+
+        public static object GetIdByName<TEntity>(DbContext context, string name) where TEntity : class
+        {
+            var nameProperty = typeof(TEntity).GetProperty("Name");
+            if (nameProperty == null)
+            {
+                throw new InvalidOperationException("Свойство 'Name' не найдено в типе " + typeof(TEntity).Name);
+            }
+
+            ParameterExpression parameter = Expression.Parameter(typeof(TEntity), "x");
+            MemberExpression propertyAccess = Expression.Property(parameter, nameProperty);
+            ConstantExpression constant = Expression.Constant(name);
+            BinaryExpression equalExpression = Expression.Equal(propertyAccess, constant);
+            Expression<Func<TEntity, bool>> lambda = Expression.Lambda<Func<TEntity, bool>>(equalExpression, parameter);
+
+            TEntity entity = context.Set<TEntity>().FirstOrDefault(lambda);
+            if (entity == null)
+            {
+                return null;
+            }
+
+            var idProperty = typeof(TEntity).GetProperty("Id");
+            if (idProperty == null)
+            {
+                throw new InvalidOperationException("Свойство 'Id' не найдено в типе " + typeof(TEntity).Name);
+            }
+
+            return idProperty.GetValue(entity);
         }
 
     }
